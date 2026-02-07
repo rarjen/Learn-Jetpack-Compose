9765685d7965b162576d02c3decaa9deac389a70
Otniel Kevin Abiel <rarjen57@gmail.com>
2026-02-07 19:44:22 +0700

    learn modifier text

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.learncompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,9 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.learncompose.ui.theme.LearnComposeTheme
@@ -41,10 +46,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LearnTextAndModifier(){
+    val context = LocalContext.current
+
+    val clickOnText = {
+        Toast.makeText(context, "Text clicked!", Toast.LENGTH_SHORT).show()
+    }
+
     Text(
-        text = stringResource(id = R.string.hello_text),
+        text = "Hello World Data Modifier",
         color = Color.Red,
         fontSize = 32.sp,
-        fontStyle = FontStyle.Italic
-    )
+        fontStyle = FontStyle.Italic,
+        modifier = Modifier.padding(22.dp).
+                    background(Color.Black).
+                    clickable(onClick = clickOnText)
+        )
+}
+
+// To preview without running the app
+@Preview(showBackground = true)
+@Composable
+fun ShowPreview(){
+    LearnComposeTheme {
+        LearnTextAndModifier()
+    }
 }
