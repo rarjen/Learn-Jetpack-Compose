efa74d4b67155becb4065e19eec38ab9d59bc2fb
Otniel Kevin Abiel <rarjen57@gmail.com>
2026-02-07 23:44:27 +0700

    learn layouting

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,7 +20,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.learncompose.ui.theme.LearnComposeTheme
 
@@ -24,7 +31,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-//        installSplashScreen()
+        installSplashScreen()
         setContent {
             LearnComposeTheme {
                 Surface(
@@ -45,10 +52,25 @@ fun LearnRowColumnBox(){
 //    Text(text = "Box")
 //    It will overlap
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
-        Text("Col 1")
-        Text("Col 2")
+//    Column
+//    Column(
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center) {
+//        Text("Col 1")
+//        Text("Col 2")
+//    }
+
+// Row
+//    Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+//        Text("row 1")
+//        Text("row 2")
+//    }
+
+//    Box Layout
+    Box(modifier = Modifier.fillMaxSize().background(Color.Yellow), contentAlignment = Alignment.Center) {
+        Box(modifier = Modifier.height(300.dp).width(300.dp).background(Color.Blue)) {
+            Text("Box", modifier = Modifier.align(Alignment.Center), Color.White, fontSize = 30.sp)
+        }
     }
+
 }
