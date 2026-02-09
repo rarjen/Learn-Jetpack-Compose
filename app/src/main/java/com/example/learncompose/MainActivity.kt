51efac41b20150dfd64299ab4d535d627e232112
Otniel Kevin Abiel <rarjen57@gmail.com>
2026-02-09 08:32:13 +0700

    learn/alignment-arrangement

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -20,7 +23,6 @@ import com.example.learncompose.ui.theme.LearnComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         installSplashScreen()
         setContent {
             LearnComposeTheme {
@@ -35,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     contentAlignment = Alignment.Center
                 ) {
-                    Greeting("Android")
+                    LearnAlignmentArrangement()
                 }
             }
         }
@@ -43,17 +45,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LearnAlignmentArrangement() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearnComposeTheme {
-        Greeting("Android")
+//   Row
+//    Row(verticalAlignment = Alignment.CenterVertically,
+//        horizontalArrangement = Arrangement.Center
+//    ) {
+//        Text("Row Alignment Arrangement")
+//    }
+
+//    Column
+//    Column(horizontalAlignment = Alignment.CenterHorizontally,
+//            verticalArrangement = Arrangement.Center
+//        ) {
+//        Text("Row Alignment Arrangement")
+//    }
+
+//    BOX
+    Box(contentAlignment = Alignment.BottomCenter) {
+        Text("TopStart", modifier = Modifier.align(Alignment.TopStart))
+        Text("TopCenter", modifier = Modifier.align(Alignment.TopCenter))
+        Text("TopEnd", modifier = Modifier.align(Alignment.TopEnd))
+        Text("BottomStart", modifier = Modifier.align(Alignment.BottomStart))
+        Text("BottomCenter", modifier = Modifier.align(Alignment.BottomCenter))
+        Text("BottomEnd", modifier = Modifier.align(Alignment.BottomEnd))
+        Text("Center", modifier = Modifier.align(Alignment.Center))
+        Text("CenterStart", modifier = Modifier.align(Alignment.CenterStart))
+        Text("CenterEnd", modifier = Modifier.align(Alignment.CenterEnd))
+
     }
 }
