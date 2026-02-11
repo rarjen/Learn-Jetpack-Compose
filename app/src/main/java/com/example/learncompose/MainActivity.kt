3976d7cbfc068f238cf46e114be3f8d9051596b7
Otniel Kevin Abiel <rarjen57@gmail.com>
2026-02-11 14:18:58 +0700

    learn list and lazy concept

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.learncompose
 
 import android.os.Bundle
+import android.view.Display
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,13 +21,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.learncompose.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         installSplashScreen()
         setContent {
             LearnComposeTheme {
@@ -30,25 +35,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val fruitList = listOf<String>("Papaya", "Watermelon", "Banana", "Strawberry", "Apple", "Papaya", "Watermelon", "Banana",
+                        "Strawberry", "Apple", "Papaya", "Watermelon", "Banana", "Strawberry", "Apple", "Papaya", "Watermelon", "Banana", "Strawberry", "Apple", "Papaya", "Watermelon", "Banana", "Strawberry", "Apple")
+
+                    DisplayList(fruitList)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LearnComposeTheme {
-        Greeting("Android")
+fun DisplayList(fruitList: List<String>) {
+//  Normal List
+//    Column() {
+//        fruitList.forEach {
+//            Text(text = " $it", fontSize = 30.sp, color = Color.Red)
+//        }
+//    }
+
+//    LazyColumn() {
+//        items(fruitList){
+//            Text(text = " $it", fontSize = 30.sp, color = Color.Red)
+//        }
+//    }
+
+    LazyRow() {
+        items(fruitList){
+            Text(text = " $it", fontSize = 30.sp, color = Color.Red)
+        }
     }
 }
