f8cc265f90dbc0a75fcf7618edb1edaeef3903d3
Otniel Kevin Abiel <rarjen57@gmail.com>
2026-02-12 15:34:53 +0700

    learn floating buttom

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.learncompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -15,8 +22,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.learncompose.ui.theme.GreenLearn
 import com.example.learncompose.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,25 +41,62 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    LearnFAB()
+                    LearnExtendedFAB()
                 }
             }
         }
     }
 }
 
+//@Composable
+//fun LearnFAB() {
+//
+//    val context = LocalContext.current.applicationContext
+//
+//    Box(
+//        modifier = Modifier.fillMaxSize()
+//    ) {
+//        FloatingActionButton(
+//            modifier = Modifier.padding(16.dp).align(Alignment.BottomEnd),
+//            containerColor = GreenLearn,
+//            onClick = {
+//                Toast.makeText(context, "FAB clicked", Toast.LENGTH_SHORT).show()
+//            }
+//        ) {
+//            Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+//        }
+//    }
+//}
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LearnExtendedFAB() {
+    val context = LocalContext.current.applicationContext
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        ExtendedFloatingActionButton(
+            modifier = Modifier.padding(16.dp).align(Alignment.BottomEnd),
+            containerColor = GreenLearn,
+            onClick = {
+                Toast.makeText(context, "Extended FAB clicked", Toast.LENGTH_SHORT).show()
+            }
+        ) {
+            Icon(imageVector = Icons.Filled.Menu, contentDescription = null)
+            Text(
+                "Menu",
+                modifier = Modifier.padding(4.dp),
+                fontSize = 16.sp
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LearnComposeTheme {
-        Greeting("Android")
+fun DefaultPreview() {
+    LearnComposeTheme() {
+        LearnExtendedFAB()
     }
 }
